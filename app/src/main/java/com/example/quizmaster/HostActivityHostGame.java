5db348602a4ebe48fbcd73d6f02e2fa033d86764
manu <emanuelkolb@gmx.de>
2025-04-24 16:03:38 +0200

    - Added HostGameActivity:   - Created `HostActivityHostGame` class.   - Added `host_activity_hostgame.xml` layout.   - Registered `HostActivityHostGame` in `AndroidManifest.xml`. - Implemented basic UI for hosting a game:   - Added UI elements for: Game name, Max players, Max points, Adding questions, Starting game.   - Added a RecyclerView for displaying questions.   - Added input fields for game name, max players, and max points.

--- app/src/main/java/com/example/quizmaster/HostActivityHostGame.java
+++ app/src/main/java/com/example/quizmaster/HostActivityHostGame.java
@@ -1,4 +1,23 @@
 package com.example.quizmaster;
 
-public class HostActivityHostGame {
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HostActivityHostGame extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        // Hier wird das Layout der Activity gesetzt
+        setContentView(R.layout.host_activity_hostgame);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
