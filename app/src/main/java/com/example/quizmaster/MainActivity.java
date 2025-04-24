25dd47a1553bfd1e2786d16d607840bb84bfce54
manu <emanuelkolb@gmx.de>
2025-04-24 15:11:09 +0200

    - button in main activity change activity - added all xml files

--- app/src/main/java/com/example/quizmaster/MainActivity.java
+++ app/src/main/java/com/example/quizmaster/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.quizmaster;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +18,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        // Buttons finden
+        Button hostButton = findViewById(R.id.HostGameButton);
+        Button playerButton = findViewById(R.id.PlayerButton);
+
+        // Listener für den Host-Button
+        hostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent zum Starten von HostActivityHostGame erstellen
+                Intent intent = new Intent(MainActivity.this, HostActivityHostGame.class);
+                startActivity(intent);
+            }
+        });
+
+        // Listener für den Player-Button
+        playerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Intent zum Starten von PlayerActivityJoinGame erstellen
+                Intent intent = new Intent(MainActivity.this, PlayerActivityJoinGame.class);
+                startActivity(intent);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
