69529902a56791d77195b92bfcce2ac0dcbbf6be
manu <emanuelkolb@gmx.de>
2025-04-25 11:12:30 +0200

    - Added `HostActivityGame`:   - Created `HostActivityGame` class.   - Added `host_activity_game.xml` layout.   - Registered `HostActivityGame` in `AndroidManifest.xml`. - Updated `host_activity_hostgame.xml`:   - Adjusted layout for `TextInputEditText`.   - Adjusted layout for `editTextNumber` and `editTextNumber2`. - Implemented basic UI for in game activity:   - Added UI elements for: Game name, Player names and their points.   - Added a Button for starting next question.   - Added a View to display the next question.

--- app/src/main/java/com/example/quizmaster/HostActivityGame.java
+++ app/src/main/java/com/example/quizmaster/HostActivityGame.java
@@ -1,4 +1,25 @@
 package com.example.quizmaster;
 
-public class HostActivityGame {
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class HostActivityGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        // Hier wird das Layout der Activity gesetzt
+        setContentView(R.layout.host_activity_game);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
 }
